de88b820e61b9945a50ff282e0488b655c30dcdd
Ahmed-Saba <ahmedm.sabaa@gmail.com>
2025-09-12 01:40:27 +0300

    Add naming convention for constraints and indexes

--- src/app/database/base.py
+++ src/app/database/base.py
@@ -1,6 +1,20 @@
+"""
+This Base class is used as the declarative base for all SQLAlchemy ORM models.
+Import this Base class in any model module that defines ORM classes.
+"""
+
 from sqlalchemy.orm import DeclarativeBase
 
-# Declarative base for SQLAlchemy models.
-# Import this Base into each models module when declaring ORM classes.
+
 class Base(DeclarativeBase):
     pass
+
+
+# Naming convention for constraints and indexes
+Base.metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
